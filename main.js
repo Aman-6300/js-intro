0d87251aca3177c7ae2b427e6a8295b59b505b9a
Aman-6300 <amansayooj03@gmail.com>
2024-08-25 09:09:20 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+document.write("hello world ");
+document.write("<br>");
+document.write("hello world ");
+console.log("this is a message");
+console.warn("this is a message");
+console.error("this is a message");
+// single line comment
+/* this is a multiple line comment */
+// let, var, const
+
+let person="anie"
+
+
+var age = 19
+console.log(age)
+
+const pi = 3.14
+console.log(pi)
